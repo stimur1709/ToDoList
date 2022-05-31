3e7ea5b40993088044ecea5a46354b5c8dddf540
stimur1709 <stimur1709@mail.ru>
2022-06-01 01:06:24 +0700

    added UserEntity and change db postgreSQL

---
+++ src/main/java/main/entity/UserEntity.java
@@ -0,0 +1,18 @@
+package main.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "users")
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+}
